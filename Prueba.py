12440b5f3b69ce1b3e59213ecac137490dfa8ceb
Julian2mai <julimaika2003@gmail.com>
2024-09-13 13:38:58 -0500

    ajuste generacion conflito repositorio local

---
+++ Prueba.py
@@ -0,0 +1,9 @@
+import time
+print("Hello") #esta linea imprime en consola la palabra hola
+print("Segundo renglon") 
+time.sleep(2)
+print("segundo renglon")
+
+print (type ("hola"))#la funcion type me dice que tipo de dato es lo que esta en los
+print (type(4))
+print(type(5.5))
